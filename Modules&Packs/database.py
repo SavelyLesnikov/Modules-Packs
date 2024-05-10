2245d19ac6d4a97cb0352f2411c90feb6f4d726a
SavelyLesnikov <166781803+SavelyLesnikov@users.noreply.github.com>
2024-05-10 20:54:38 +0700

    Add files via upload

---
+++ Modules&Packs/database.py
@@ -0,0 +1,6 @@
+def greetings1():
+    print('Hello, World')
+
+
+def greetings2():
+    print('Hello, World! ' * 3)
---
+++ Modules&Packs/main.py
@@ -0,0 +1,5 @@
+from database import greetings1, greetings2 as gr2
+print('***')
+greetings1()
+print('***')
+gr2()
